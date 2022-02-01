b840871f6a020ffa8c599e97d05a2fff6c7293cc
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-01 19:30:50 +0900

    youtubeAPIからデータ取得

---
+++ src/apis/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const APIKEY = process.env.REACT_APP_YOUTUBEAPI_KEY;
+
+const youtube = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+});
+
+export const fetchPopularData = async () => {
+  return await youtube.get("/videos", {
+    params: {
+      part: "snippet",
+      maxResult: 40,
+      key: APIKEY,
+      regionCode: "JP",
+      type: "video",
+      chart: "mostPopular",
+    },
+  });
+};
